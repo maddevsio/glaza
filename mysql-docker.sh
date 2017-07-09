45479be08238db355846229a6533789e9d6d9427
puzanov <puzanov@gmail.com>
2017-07-09 15:40:57 +0600

    added commans for stop and remove container before run

--- mysql-docker.sh
+++ mysql-docker.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 #docker pull mysql
+docker stop mysql
+docker rm mysql
 docker run -p 3306:3306 --name mysql -v /usr/local/opt/docker-volumes/mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD= -d mysql:5.7
