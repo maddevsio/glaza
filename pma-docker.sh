a87a257dfa73c1c451795748070c477f550c2b43
puzanov <puzanov@gmail.com>
2017-07-09 16:28:54 +0600

    pma port changed

--- pma-docker.sh
+++ pma-docker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 docker stop pma
 docker rm pma
-docker run --name pma -d --link mysql:db -p 8080:80 phpmyadmin/phpmyadmin
+docker run --name pma -d --link mysql:db -p 8090:80 phpmyadmin/phpmyadmin
 docker ps -a
 sleep 2
 docker logs pma
