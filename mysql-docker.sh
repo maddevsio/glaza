fd3c1c622343daf74dcc83eaedb5d9a8cca58a72
puzanov <puzanov@gmail.com>
2017-07-09 15:39:30 +0600

    port added

--- mysql-docker.sh
+++ mysql-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker pull mysql
-docker run --name mysql -v /usr/local/opt/docker-volumes/mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD= -d mysql:5.7
+docker run -p 3306:3306 --name mysql -v /usr/local/opt/docker-volumes/mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD= -d mysql:5.7
