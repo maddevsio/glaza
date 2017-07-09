47ecbbc5cf47d85cb33c6d9c3cd6238010906d99
puzanov <puzanov@gmail.com>
2017-07-09 15:18:08 +0600

    whitespace removed

--- mysql-docker.sh
+++ mysql-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker pull mysql
-docker run --name mysql -v /usr/local/opt/docker-volumes/mysql:/var/lib/mysql  -e MYSQL_ROOT_PASSWORD= -d mysql:5.7
+docker run --name mysql -v /usr/local/opt/docker-volumes/mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD= -d mysql:5.7
