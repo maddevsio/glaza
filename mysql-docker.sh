6d8d53beb7526501ac71186a4ba27887ad820495
puzanov <puzanov@gmail.com>
2017-07-09 15:47:34 +0600

    added sleep before logs

--- mysql-docker.sh
+++ mysql-docker.sh
@@ -3,5 +3,6 @@
 docker stop mysql
 docker rm mysql
 docker run -p 3306:3306 --name mysql -v /usr/local/opt/docker-volumes/mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=root -d mysql:5.7
-docker logs mysql
 docker ps
+sleep 2
+docker logs mysql
