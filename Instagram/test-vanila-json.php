36b381809b9c77973879f3155f608679c33f329a
puzanov <puzanov@gmail.com>
2017-07-09 19:44:16 +0600

    added db function added simple and dirty code for data saving
    to be rafactored

--- Instagram/test-vanila-json.php
+++ Instagram/test-vanila-json.php
@@ -6,8 +6,10 @@
  * Time: 7:55 PM
  */
 
+include_once "../lib/db.php";
+
 $url = "https://www.instagram.com/hippohae/?__a=1";
 $json = file_get_contents($url);
 $data = json_decode($json);
-var_dump($data);
 var_dump($data->user->followed_by);
+saveLog("instagram_hippohae_followed_by", strval($data->user->followed_by->count));
--- lib/db.php
+++ lib/db.php
@@ -6,3 +6,24 @@
  * Time: 2:38 PM
  */
 
+function saveLog($type, $value) {
+    $mysqli = new mysqli("127.0.0.1", "root", "root", "glaza");
+
+    if (mysqli_connect_errno()) {
+        printf("Mysql connection error: %s\n", mysqli_connect_error());
+        exit();
+    }
+
+    echo "Server info: " . $mysqli->host_info . PHP_EOL;
+
+    $query = "INSERT INTO log (type, value) VALUES (?,?)";
+    $stmt = $mysqli->prepare($query);
+    $stmt->bind_param("ss", $typeBind, $valueBind);
+    $typeBind = $type;
+    $valueBind = $value;
+    if (!$stmt->execute()) {
+        var_dump($stmt->error);
+    }
+    $stmt->close();
+    $mysqli->close();
+}
