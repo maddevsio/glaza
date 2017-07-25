25dc66d7c69440436171a89f666ed7d04c4b854a
puzanov <puzanov@gmail.com>
2017-07-25 13:31:18 +0600

    added docker compose, redundant filed deleted, schema moved to lib

--- mysql-docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#docker pull mysql
-docker stop mysql
-docker rm mysql
-docker run -p 3306:3306 --name mysql -v /usr/local/opt/docker-volumes/mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=root -d mysql:5.7
-docker ps
-sleep 2
-docker logs mysql
--- pma-docker.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-docker stop pma
-docker rm pma
-docker run --name pma -d --link mysql:db -p 8090:80 phpmyadmin/phpmyadmin
-docker ps -a
-sleep 2
-docker logs pma
