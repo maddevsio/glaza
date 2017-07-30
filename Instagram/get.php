fc06bacae3680e2d9e1184bcadbfdd0ee0e17c1f
puzanov <puzanov@gmail.com>
2017-07-30 15:33:43 +0600

    support of multiple instagram accounts

--- Instagram/get.php
+++ Instagram/get.php
@@ -13,34 +13,36 @@ include_once "db.php";
 while (true) {
   $mysqli = getDB();
 
-  $account = 'hippohae';
-  $url = "https://www.instagram.com/$account/?__a=1";
+  $accounts = array("silkroadexplore", "hippohae");
 
-  $json = file_get_contents($url);
-  $data = json_decode($json);
+  foreach ($accounts as $account) {
+    $url = "https://www.instagram.com/$account/?__a=1";
 
-  print("Instagram followed_by " . $data->user->followed_by->count . "\n");
-  print("Instagram follows " . $data->user->follows->count . "\n");
-  print("Instagram photos " . $data->user->media->count . "\n");
+    $json = file_get_contents($url);
+    $data = json_decode($json);
 
-  $query = "INSERT INTO instagram (account,followed_by,follows,photos) VALUES (?,?,?,?)";
-  if ($stmt = $mysqli->prepare($query)) {
-    $stmt->bind_param("siii",
-      $account,
-      $data->user->followed_by->count,
-      $data->user->follows->count,
-      $data->user->media->count
-    );
-    if (!$stmt->execute()) {
-        var_dump($stmt->error);
+    print("Instagram followed_by " . $data->user->followed_by->count . "\n");
+    print("Instagram follows " . $data->user->follows->count . "\n");
+    print("Instagram photos " . $data->user->media->count . "\n");
+
+    $query = "INSERT INTO instagram (account,followed_by,follows,photos) VALUES (?,?,?,?)";
+    if ($stmt = $mysqli->prepare($query)) {
+      $stmt->bind_param("siii",
+        $account,
+        $data->user->followed_by->count,
+        $data->user->follows->count,
+        $data->user->media->count
+      );
+      if (!$stmt->execute()) {
+          var_dump($stmt->error);
+      } else {
+        print("Saved\n");
+      }
+      $stmt->close();
     } else {
-      print("Saved\n");
+      var_dump($mysqli->error);
     }
-    $stmt->close();
-    $mysqli->close();
-  } else {
-    var_dump($mysqli->error);
   }
-
+  $mysqli->close();
   sleep(60);
 }
