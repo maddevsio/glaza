6597f3041bea37c80778d5fa5e932a07135dd384
puzanov <puzanov@gmail.com>
2018-01-19 23:53:26 +0600

    deleted mysql integration, added mongo in compose

--- Instagram/get.php
+++ Instagram/get.php
@@ -6,13 +6,7 @@
  * Time: 7:55 PM
  */
 
-set_include_path(get_include_path() . PATH_SEPARATOR . '../lib' . PATH_SEPARATOR . './lib');
-
-include_once "db.php";
-
 while (true) {
-  $mysqli = getDB();
-
   $accounts = array();
   if (getenv('INSTAGRAM_ACCOUNTS')) {
     $accounts = explode(',', getenv('INSTAGRAM_ACCOUNTS'));
@@ -30,25 +24,7 @@ while (true) {
     print("Instagram followed_by " . $data->user->followed_by->count . "\n");
     print("Instagram follows " . $data->user->follows->count . "\n");
     print("Instagram photos " . $data->user->media->count . "\n");
-
-    $query = "INSERT INTO instagram (account,followed_by,follows,photos) VALUES (?,?,?,?)";
-    if ($stmt = $mysqli->prepare($query)) {
-      $stmt->bind_param("siii",
-        $account,
-        $data->user->followed_by->count,
-        $data->user->follows->count,
-        $data->user->media->count
-      );
-      if (!$stmt->execute()) {
-          var_dump($stmt->error);
-      } else {
-        print("Saved\n");
-      }
-      $stmt->close();
-    } else {
-      var_dump($mysqli->error);
-    }
   }
-  $mysqli->close();
+  
   sleep(60);
 }
