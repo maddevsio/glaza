f8d79b4d76ffa099ae03730ab5a337e0cdd37527
Oleg Puzanov <puzanov@gmail.com>
2018-03-06 18:38:05 +0600

    makefile added, ready for prod

--- GoogleDocs/src/googlesheets/main.go
+++ GoogleDocs/src/googlesheets/main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"googlesheets/lib"
 	"log"
+	"os"
 )
 
 func main() {
 	log.Print("Empty logic")
 	tab := lib.NewTab()
+	tab.SpreadsheetID = os.Getenv("GOOGLE_SHEET_ID")
+	tab.SecretFile = os.Getenv("SECRET_FILE")
+	tab.Name = os.Getenv("TAB_NAME")
 	log.Print(tab.GetLastRecord())
 }
