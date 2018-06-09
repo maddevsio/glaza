f7c4389203dd3eecddf2b8e3995d134c31e9bb37
Oleg Puzanov <puzanov@gmail.com>
2018-06-09 17:06:23 +0600

    added storage feature to the main.go

--- GoogleDocs/src/googlesheets/main.go
+++ GoogleDocs/src/googlesheets/main.go
@@ -4,13 +4,26 @@ import (
 	"googlesheets/lib"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
-	log.Print("Empty logic")
 	tab := lib.NewTab()
 	tab.SpreadsheetID = os.Getenv("GOOGLE_SHEET_ID")
 	tab.SecretFile = os.Getenv("SECRET_FILE")
 	tab.Name = os.Getenv("TAB_NAME")
-	log.Print(tab.GetLastRecord())
+
+	items, err := tab.GetLastRecord()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	storage := lib.NewStorageItem()
+	storage.Name = tab.Name
+	storage.Collection = "docs"
+	storage.Value = strings.Join(items, " ")
+	err = storage.Save()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
