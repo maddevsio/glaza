ad61f8ea936e21d4ebbe4f110824b8cf9cf46b12
Oleg Puzanov <puzanov@gmail.com>
2018-06-19 18:36:21 +0600

    main logic extracted, ready to calcilate dates

--- GoogleWebmasters/src/googlewebmasters/lib/api.go
+++ GoogleWebmasters/src/googlewebmasters/lib/api.go
@@ -1,1 +1,53 @@
 package lib
+
+import (
+	"context"
+	"io/ioutil"
+
+	"github.com/go-resty/resty"
+	"golang.org/x/oauth2/google"
+)
+
+// API is a struct for Google Webmasters API
+// URL goes in this format: https:example.com,
+// because of a bug in resty: https://github.com/go-resty/resty/issues/159
+type API struct {
+	SecretFile string
+	URL        string
+}
+
+// NewAPI is a API struct constructor
+func NewAPI() API {
+	return API{}
+}
+
+// GetDataForNDays returns search analytics for a number of days passed as a param
+func (a *API) GetDataForNDays(days int) (string, error) {
+	data, err := ioutil.ReadFile(a.SecretFile)
+	if err != nil {
+		return "", err
+	}
+
+	conf, err := google.JWTConfigFromJSON(data, "https://www.googleapis.com/auth/webmasters.readonly")
+	if err != nil {
+		return "", err
+	}
+
+	client := conf.Client(context.TODO())
+	rc := resty.NewWithClient(client)
+	restyResp, err := rc.R().
+		SetBody(map[string]interface{}{"startDate": "2018-06-01", "endDate": "2018-06-18", "dimensions": []string{"date"}}).
+		Post("https://www.googleapis.com/webmasters/v3/sites/" + a.URL + "%2F/searchAnalytics/query")
+
+	return restyResp.String(), err
+}
+
+// GetCurrentDate returns current date in format 2018-31-12
+func GetCurrentDate() string {
+	return "2018"
+}
+
+// GetDateNDaysBefore returns date in format 2018-31-12 minus N days from current date
+func GetDateNDaysBefore(days int) string {
+	return "2039"
+}
--- GoogleWebmasters/src/googlewebmasters/lib/api_test.go
+++ GoogleWebmasters/src/googlewebmasters/lib/api_test.go
@@ -1,31 +1,18 @@
 package lib
 
 import (
-	"context"
-	"io/ioutil"
+	"encoding/json"
 	"testing"
 
-	"github.com/go-resty/resty"
 	"github.com/stretchr/testify/assert"
-	"golang.org/x/oauth2/google"
 )
 
 func TestApi(t *testing.T) {
-	data, err := ioutil.ReadFile("client_secret.json")
+	api := NewAPI()
+	api.SecretFile = "client_secret.json"
+	api.URL = "https:silkroadexplore.com"
+	jsonData, err := api.GetDataForNDays(0)
 	assert.NoError(t, err)
-
-	conf, err := google.JWTConfigFromJSON(data, "https://www.googleapis.com/auth/webmasters.readonly")
-	assert.NoError(t, err)
-
-	client := conf.Client(context.TODO())
-
-	rc := resty.NewWithClient(client)
-	restyResp, err := rc.R().
-		SetBody(map[string]interface{}{"startDate": "2018-06-01", "endDate": "2018-06-18", "dimensions": []string{"date"}}).
-		Post("https://www.googleapis.com/webmasters/v3/sites/https:silkroadexplore.com%2F/searchAnalytics/query")
-	assert.NoError(t, err)
-	t.Log(restyResp.StatusCode())
-	t.Log(restyResp.String())
-
-	t.Fail()
+	assert.True(t, json.Valid([]byte(jsonData)))
+	t.Log(jsonData)
 }
