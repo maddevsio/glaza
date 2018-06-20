6f57ac7c2624fb0ad7e23768527ec15e7a6e1001
Oleg Puzanov <puzanov@gmail.com>
2018-06-20 11:17:21 +0600

    api is done

--- GoogleWebmasters/src/googlewebmasters/lib/api.go
+++ GoogleWebmasters/src/googlewebmasters/lib/api.go
@@ -3,6 +3,8 @@ package lib
 import (
 	"context"
 	"io/ioutil"
+	"net/http"
+	"time"
 
 	"github.com/go-resty/resty"
 	"golang.org/x/oauth2/google"
@@ -21,33 +23,57 @@ func NewAPI() API {
 	return API{}
 }
 
-// GetDataForNDays returns search analytics for a number of days passed as a param
-func (a *API) GetDataForNDays(days int) (string, error) {
-	data, err := ioutil.ReadFile(a.SecretFile)
+// GetClicksForNDays returns search analytics for a number of days passed as a param
+// https://developers.google.com/webmaster-tools/search-console-api-original/v3/how-tos/search_analytics
+func (a *API) GetClicksForNDays(days int) (string, error) {
+	client, err := a.GetOAuthClient()
 	if err != nil {
 		return "", err
 	}
-
-	conf, err := google.JWTConfigFromJSON(data, "https://www.googleapis.com/auth/webmasters.readonly")
-	if err != nil {
-		return "", err
-	}
-
-	client := conf.Client(context.TODO())
 	rc := resty.NewWithClient(client)
 	restyResp, err := rc.R().
-		SetBody(map[string]interface{}{"startDate": "2018-06-01", "endDate": "2018-06-18", "dimensions": []string{"date"}}).
+		SetBody(map[string]interface{}{"startDate": GetDateNDaysBefore(days), "endDate": GetCurrentDate(), "dimensions": []string{"date"}}).
 		Post("https://www.googleapis.com/webmasters/v3/sites/" + a.URL + "%2F/searchAnalytics/query")
 
 	return restyResp.String(), err
 }
 
+// GetQueriesForNDays returns search analytics for a number of days passed as a param
+func (a *API) GetQueriesForNDays(days int) (string, error) {
+	client, err := a.GetOAuthClient()
+	if err != nil {
+		return "", err
+	}
+	rc := resty.NewWithClient(client)
+	restyResp, err := rc.R().
+		SetBody(map[string]interface{}{"startDate": GetDateNDaysBefore(days), "endDate": GetCurrentDate(), "dimensions": []string{"query"}}).
+		Post("https://www.googleapis.com/webmasters/v3/sites/" + a.URL + "%2F/searchAnalytics/query")
+
+	return restyResp.String(), err
+}
+
+// GetOAuthClient returns valid http.client ready to work with google api
+func (a *API) GetOAuthClient() (*http.Client, error) {
+	data, err := ioutil.ReadFile(a.SecretFile)
+	if err != nil {
+		return nil, err
+	}
+
+	conf, err := google.JWTConfigFromJSON(data, "https://www.googleapis.com/auth/webmasters.readonly")
+	if err != nil {
+		return nil, err
+	}
+
+	client := conf.Client(context.TODO())
+	return client, nil
+}
+
 // GetCurrentDate returns current date in format 2018-31-12
 func GetCurrentDate() string {
-	return "2018"
+	return time.Now().Format("2006-01-02")
 }
 
 // GetDateNDaysBefore returns date in format 2018-31-12 minus N days from current date
 func GetDateNDaysBefore(days int) string {
-	return "2039"
+	return time.Now().AddDate(0, 0, -days).Format("2006-01-02")
 }
--- GoogleWebmasters/src/googlewebmasters/lib/api_test.go
+++ GoogleWebmasters/src/googlewebmasters/lib/api_test.go
@@ -7,12 +7,35 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestApi(t *testing.T) {
+func TestGetClicksForNDays(t *testing.T) {
 	api := NewAPI()
 	api.SecretFile = "client_secret.json"
 	api.URL = "https:silkroadexplore.com"
-	jsonData, err := api.GetDataForNDays(0)
+	jsonData, err := api.GetClicksForNDays(30)
 	assert.NoError(t, err)
 	assert.True(t, json.Valid([]byte(jsonData)))
+	assert.Contains(t, jsonData, "impressions")
+	assert.Contains(t, jsonData, "clicks")
 	t.Log(jsonData)
+}
+
+func TestGetQueriesForNDays(t *testing.T) {
+	api := NewAPI()
+	api.SecretFile = "client_secret.json"
+	api.URL = "https:silkroadexplore.com"
+	jsonData, err := api.GetQueriesForNDays(30)
+	assert.NoError(t, err)
+	assert.True(t, json.Valid([]byte(jsonData)))
+	assert.Contains(t, jsonData, "keys")
+	assert.Contains(t, jsonData, "impressions")
+	assert.Contains(t, jsonData, "clicks")
+	t.Log(jsonData)
+}
+
+func TestDate(t *testing.T) {
+	tf := GetCurrentDate()
+	assert.Contains(t, tf, "2018")
+	tf = GetDateNDaysBefore(30)
+	assert.Contains(t, tf, "2018") // lame
+	t.Log(tf)
 }
