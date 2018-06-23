60fd1ab0b107415da73b094fbfec704d6aaa0823
Oleg Puzanov <puzanov@gmail.com>
2018-06-23 18:06:47 +0600

    added clicks and queries to the morda

---
+++ morda/src/components/webmasters/WebmastersQueries.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+
+class WebmastersQueries extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:8080/glaza/webmasters?pagesize=1&sort=-date&filter=%7B%22value%22%3A%20%22queries%22%7D")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result._embedded
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+    const { error, isLoaded, items } = this.state;  
+    return (
+      <div>
+        {error && <div>Error: {error.message}</div>}
+        {items.map(item => (
+          <div>
+            <b>{item.name} {item.value}</b>
+            <table>
+            {item.json.rows.map(row => (
+              <tr>
+                <td>{row.clicks}</td>
+                <td>{row.ctr}</td>
+                <td>{row.impressions}</td>
+                <td>{row.position}</td>
+                <td>{row.keys[0]}</td>
+              </tr>
+            ))}
+            </table>
+          </div>
+        ))}
+      </div>
+    );   
+  }
+}
+
+export default WebmastersQueries;
--- morda/src/index.js
+++ morda/src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Docs from './components/docs/Docs';
 import PageInsights from './components/pageinsights/PageInsights';
+import WebmastersClicks from './components/webmasters/WebmastersClicks';
+import WebmastersQueries from './components/webmasters/WebmastersQueries';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Docs />, document.getElementById('docs'));
 ReactDOM.render(<PageInsights />, document.getElementById('pageinsights'));
+ReactDOM.render(<WebmastersClicks />, document.getElementById('webmasters-clicks'));
+ReactDOM.render(<WebmastersQueries />, document.getElementById('webmasters-queries'));
 
 registerServiceWorker();
