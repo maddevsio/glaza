32d39cedbff1b5b96c670e2bb7c8c0347d25a950
Oleg Puzanov <puzanov@gmail.com>
2018-12-22 23:48:24 +0600

    added instagram feature, fully working

--- Instagram/parser.py
+++ Instagram/parser.py
@@ -1,34 +1,42 @@
+#!/usr/bin/python3.6
+
+import sys
+import datetime
 from instaparser.agents import Agent
 from instaparser.entities import Account, Media
 from pymongo import MongoClient
-
-client = MongoClient('localhost', 27017)
-db = client.glaza
-
-instagram_users_data = db.instagram_users_data
 
 def get_user_data(username):
     agent = Agent()
     account = Account(username)
 
-    media, pointer = agent.get_media(account, count=3)
+    medias, _ = agent.get_media(account, count=1)
     follows = account.follows_count
     followers = account.followers_count
     media_count = account.media_count
+    
     media_urls = []
-    if media:
-        for i in range(2):
-            media_urls.append("https://www.instagram.com/p/" + str(media[i]))
+
+    if medias:
+        for media in medias:
+            media_urls.append(media.display_url)
 
     user_data = {
             'username':username,
             'media_urls': media_urls,
             'follows': follows,
             'followed_by': followers,
-            'media_count': media_count
+            'media_count': media_count,
+            'date': datetime.datetime.utcnow()
         }
 
     return user_data
 
-def user_info(username, db=instagram_users_data):
-    db.insert_one(get_user_data(username))
+def save_user_data(username, collection):
+    collection.insert_one(get_user_data(username))
+
+client = MongoClient('mongodb', 27017)
+db = client['glaza']
+instagram_collection = db.instagram
+save_user_data(sys.argv[1], instagram_collection)
+print("saved " + sys.argv[1])
